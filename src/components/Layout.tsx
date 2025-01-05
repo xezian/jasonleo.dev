2a967ec24cec852240fdc8f2376204448f4744d1
Jason A Leo <30601823+xezian@users.noreply.github.com>
2025-01-05 10:13:07 -0500

    Add motion

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,35 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Link } from "react-router";
 import { Outlet } from "react-router";
 import waves from "../assets/svg/waves.svg";
 import book from "../assets/svg/book-image.svg";
+import { AnimatePresence, useAnimate, usePresence } from "motion/react";
 
 export const Layout: FC = () => {
+  const [menuHovered, setMenuHovered] = useState(false);
+
+  const menuIconEnter = () => {
+    if (!menuHovered) {
+      setMenuHovered(true);
+    }
+  };
+  const menuIconLeave = () => setMenuHovered(false);
+
   return (
     <>
       <div className="p-2 w-lvh h-lvh flex flex-wrap gap-2 bg-gray-200">
         <div className="w-full h-full flex flex-col">
-          <div className="flex  p-1 mb-2 rounded-md border bg-white">
+          <div
+            onMouseEnter={menuIconEnter}
+            onMouseLeave={menuIconLeave}
+            className="flex w-fit p-1 mb-2 rounded-md border bg-white"
+          >
             <Link to="/">
               <img src={waves} alt="Waves" />
             </Link>
-            <Link to="/premises">
-              <img src={book} alt="Book" />
-            </Link>
+            <AnimatePresence mode="wait" initial={false}>
+              {menuHovered ? <MenuExpanded key="menuExpanded" /> : null}
+            </AnimatePresence>
           </div>
           <div className="flex-1">
             <Outlet />
@@ -23,5 +37,34 @@ export const Layout: FC = () => {
         </div>
       </div>
     </>
+  );
+};
+
+const MenuExpanded = () => {
+  const [isPresent, safeToRemove] = usePresence();
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    if (isPresent) {
+      const enterAnimation = () => {
+        animate(scope.current, { width: "fit-content", height: "100%" });
+      };
+      enterAnimation();
+    } else {
+      const exitAnimation = async () => {
+        await animate(scope.current, { width: "0px", height: "100%" });
+        safeToRemove();
+      };
+
+      exitAnimation();
+    }
+  });
+
+  return (
+    <div ref={scope} className="w-0">
+      <Link className="overflow-visible" to="/premises">
+        <img src={book} alt="Book" />
+      </Link>
+    </div>
   );
 };
