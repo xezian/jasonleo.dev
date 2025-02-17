a8612e8908391e59ac74543a8a7be0738f75d7e9
Jason A Leo <30601823+xezian@users.noreply.github.com>
2025-02-17 15:39:28 -0500

    Something doing something

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,20 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Home, Observations, Premises } from "./pages";
 import { Layout } from "./components";
+import { LazyMotion, domAnimation } from "motion/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="jasonleo.dev">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="premises" element={<Premises />} />
-          <Route path="observations" element={<Observations />} />
-        </Route>
-      </Routes>
+      <LazyMotion features={domAnimation} strict>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="premises" element={<Premises />} />
+            <Route path="observations" element={<Observations />} />
+          </Route>
+        </Routes>
+      </LazyMotion>
     </BrowserRouter>
   </StrictMode>
 );
--- src/pages/Observations.tsx
+++ src/pages/Observations.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import * as m from "motion/react-m";
 import HeadManager from "../components/HeadManager";
 
 import cone from "../assets/svg/cone.svg";
@@ -8,8 +9,20 @@ export const Observations: FC = () => {
     <>
       <HeadManager title="Jason Leo - Observations" favicon={cone} />
       <div className="p-1 h-full w-full flex flex-wrap gap-2 bg-black rounded-md">
-        <div className="rounded-md border bg-rose-200 flex w-full flex-col justify-end items-center">
-          <img src={cone} alt="cone icon" className="mb-[50vh]" />
+        <div className="rounded-md border bg-rose-200 text-pink-500 flex w-full flex-col justify-end items-center">
+          <m.div
+            className="mb-[50vh]"
+            whileHover={{
+              rotate: 360,
+              transition: {
+                repeat: Infinity,
+                duration: 0.4,
+                ease: "linear",
+              },
+            }}
+          >
+            <img src={cone} alt="cone-icon" />
+          </m.div>
         </div>
       </div>
     </>
